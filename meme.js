e6e38360bb921b7b1b2decad2764bd741dc782e4
Camila <camiska922@gmai.com>
2023-03-26 18:47:21 -0300

    añade filtros funcionando y cambio de aside

--- meme.js
+++ meme.js
@@ -3,46 +3,66 @@ const $ = (selector) => document.querySelector(selector);
 $(".btn-i").addEventListener("click", () => {
   const currentTheme = $("body").getAttribute("data-theme");
   if (currentTheme === "light-theme") {
-    console.log($(".fa-lightbulb"));
     $(".fa-lightbulb").classList.remove("fa-solid");
     $(".fa-lightbulb").classList.add("fa-regular");
     $("body").removeAttribute("data-theme", "light-theme");
+    $(".btn-i").innerHTML = $(".btn-i").innerHTML.replace("claro", "oscuro")
   } else {
     $(".fa-lightbulb").classList.remove("fa-regular");
     $(".fa-lightbulb").classList.add("fa-solid");
     $("body").setAttribute("data-theme", "light-theme");
+    $(".btn-i").innerHTML = $(".btn-i").innerHTML.replace("oscuro", "claro")
   }
 });
 
 //url
-$(".url-img"),addEventListener("input",()=> {
+$(".url-img"), addEventListener("input", () => {
   const urlImage = $(".url-img").value
   $("#img-meme").style.background = `url(${urlImage})`
 })
 
-$(".color"),addEventListener("input",() => {
+$(".color"), addEventListener("input", () => {
   $("#img-meme").style.backgroundColor = $(".color").value
 })
 
+
+
 //filtros
 
-/*const setFilterValue = (value) =>
- (${value}%);*/
+const setFilterValue = () => {
+  const brightness = $("#brillo").value
+  const opacity = $("#opacidad").value
+  const contrast = $("#contraste").value
+  const blurPx = $("#desenfoque").value
+  const grayscale = $("#escala-de-grises").value
+  const sepia = $("#sepia").value
+  const hueRotateDeg = $("#hue").value
+  const saturate = $("#saturado").value
+  const invert = $("#negativo").value
 
-/*
-$("#brillo"),addEventListener("input",() => {
-  const setFilterValue = $("#brillo").value
-  $("#img-meme").style.filter = `brightness(${setFilterValue}%)`
-})
+  const filters = `brightness(${brightness}%) opacity(${opacity}%) contrast(${contrast}%) blur(${blurPx}px) grayscale(${grayscale}%) sepia(${sepia}%) hue-rotate(${hueRotateDeg}deg) saturate(${saturate}%) invert(${invert}%)`
 
-
-$("#opacidad"),addEventListener("input",() => {
-  const setFilterValue2 = $("#opacidad").value
-  $("#img-meme").style.filter = `opacity(${setFilterValue2}%)`
-})
-*/
-
-const setFilterValue= () => {
-  const brightness = $(`brillo`).value
+  $("#img-meme").style.filter = filters
 }
-$("#img-meme").style.filter =`brightness(${brillo})`
+
+$("[type=range]"), addEventListener("input", () => {
+  setFilterValue()
+})
+
+
+
+
+
+
+
+//aside texto
+
+$("#btn-text").addEventListener("click", () => {
+  $(".text").style.display = "block";
+  $(".img-column").style.display = "none";
+})
+
+$("#btn-img").addEventListener("click", () => {
+  $(".text").style.display = "none";
+  $(".img-column").style.display = "block";
+})
